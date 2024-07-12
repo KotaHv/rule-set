e831d9354760ced07870893ddcecfff3810a99d6
KotaHv <92137267+KotaHv@users.noreply.github.com>
2024-07-12 18:17:31 +0800

    fix: Adjust the generation order

--- src/generator/egern.py
+++ src/generator/egern.py
@@ -19,10 +19,10 @@ class EgernGenerator:
         yaml_data = {"no_resolve": self.info.no_resolve}
         if self.rules.domain:
             yaml_data["domain_set"] = self.rules.domain
-        if self.rules.domain_keyword:
-            yaml_data["domain_keyword_set"] = self.rules.domain_keyword
         if self.rules.domain_suffix:
             yaml_data["domain_suffix_set"] = self.rules.domain_suffix
+        if self.rules.domain_keyword:
+            yaml_data["domain_keyword_set"] = self.rules.domain_keyword
         if self.rules.ip_cidr:
             yaml_data["ip_cidr_set"] = self.rules.ip_cidr
         if self.rules.ip_cidr6:
