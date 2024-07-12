f309ce3f005d1630dcf646be63aeba858968614c
KotaHv <92137267+KotaHv@users.noreply.github.com>
2024-07-12 18:13:34 +0800

    fix: Delete redundant list methods

--- src/generator/egern.py
+++ src/generator/egern.py
@@ -18,17 +18,17 @@ class EgernGenerator:
     def generate(self):
         yaml_data = {"no_resolve": self.info.no_resolve}
         if self.rules.domain:
-            yaml_data["domain_set"] = list(self.rules.domain)
+            yaml_data["domain_set"] = self.rules.domain
         if self.rules.domain_keyword:
-            yaml_data["domain_keyword_set"] = list(self.rules.domain_keyword)
+            yaml_data["domain_keyword_set"] = self.rules.domain_keyword
         if self.rules.domain_suffix:
-            yaml_data["domain_suffix_set"] = list(self.rules.domain_suffix)
+            yaml_data["domain_suffix_set"] = self.rules.domain_suffix
         if self.rules.ip_cidr:
-            yaml_data["ip_cidr_set"] = list(self.rules.ip_cidr)
+            yaml_data["ip_cidr_set"] = self.rules.ip_cidr
         if self.rules.ip_cidr6:
-            yaml_data["ip_cidr6_set"] = list(self.rules.ip_cidr6)
+            yaml_data["ip_cidr6_set"] = self.rules.ip_cidr6
         if self.rules.ip_asn:
-            yaml_data["asn_set"] = list(self.rules.ip_asn)
+            yaml_data["asn_set"] = self.rules.ip_asn
         if len(yaml_data.keys()) > 1:
             with self.path.open("w") as f:
                 yaml.dump(yaml_data, f, sort_keys=False)
